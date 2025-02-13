6c4d7d329d6dde1810ac8fccedfc93d6a78379e8
Sreeju KS <74912840+sreesreejuks@users.noreply.github.com>
2025-02-13 12:23:14 +0530

    typo

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-sudo apt-get insatll cowsay -y
+sudo apt-get install cowsay -y
 cowsay -f dragon "Run fro cover, I am a DRAGON....RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 ls -ltra
